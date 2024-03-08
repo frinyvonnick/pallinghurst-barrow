72f76adc880f63d4046963bb6aaad1d31faabc89
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-03-08 08:21:05 +0100

    Move things around

--- components/state_machine/seek_state.gd
+++ components/state_machine/seek_state.gd
@@ -4,6 +4,7 @@ extends State
 @export var navigationAgent: NavigationAgent2D
 @export var speed: float = 100
 @export var range: float = 24
+@export var distance_target: float = 10
 @export var skills: Array[Skill]
 
 var time_elapsed = 0
@@ -15,6 +16,8 @@ func enter(_msg := {}):
 		return _transition_to_idle()
 	super()
 	target = _msg.get('target')
+	# sort skills by range
+	skills.sort_custom(sort_skills_by_range_ascending)
 	navigationAgent.velocity_computed.connect(_on_velocity_computed)
 	_set_target_position()
 	
@@ -39,7 +42,9 @@ func physics_update(delta):
 		# To avoid obstacles
 		navigationAgent.velocity = direction * speed
 	else:
-		_on_velocity_computed(direction * speed)
+		# Prevent to do micro movements to adjust position
+		if (navigationAgent.distance_to_target() > distance_target):
+			_on_velocity_computed(direction * speed)
 	
 	_check_can_attack()
 
@@ -56,11 +61,19 @@ func _transition_to_idle():
 	
 func _transition_to_attack(skill):
 	state_machine.transition_to('attack_state', {skill = skill})
-	
+
+func sort_skills_by_range_ascending(a: Skill, b: Skill):
+		if a.range < b.range:
+			return true
+		return false	
+
 func _set_target_position():
-	navigationAgent.target_position = target.global_position
+	var direction = (actor.global_position - target.global_position).normalized()
+	var shortestSkillRange = skills[0].range
+	
+	navigationAgent.target_position = target.global_position + (direction * (shortestSkillRange * 0.7))
 
 func _check_can_attack():
 	for skill in skills:
-		if (skill.can_use(navigationAgent.target_position)):
+		if (skill.can_use(target.global_position)):
 			_transition_to_attack(skill)
