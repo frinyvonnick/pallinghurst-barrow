342991f5184892f5b146f244bbf40525170b897e
Yvonnick FRIN <frin.yvonnick@gmail.com>
2024-03-14 01:09:29 +0100

    Delete unused files

--- levels/test/sandbox.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var lines: Array[String] = ["Bonjour !", "ça va ?"]
-	DialogManager.start_dialog(lines)
